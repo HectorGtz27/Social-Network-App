4ab9a1353d452bc08663fe60dc529b09c0ba7399
HectorGtz27 <alan.gtz51@gmail.com>
2024-10-17 01:37:32 -0700

    Refactor import paths for context files and navigation

--- App.js
+++ App.js
@@ -1,25 +1,11 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { AuthProvider } from "./contexts/AuthContext"; // Importa el AuthProvider
-import LoginScreen from "./screens/LoginScreen";
-import SignUpScreen from "./screens/SignUpScreen";
-import HomeScreen from "./screens/HomeScreen";
-import ShareScreen from "./screens/ShareScreen";
-
-const Stack = createStackNavigator();
+import { AuthProvider } from "./contexts/AuthContext";
+import AppNavigator from "./navigation/AppNavigator";
 
 const App = () => {
   return (
     <AuthProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="SignUp" component={SignUpScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Share" component={ShareScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <AppNavigator />
     </AuthProvider>
   );
 };
---
+++ navigation/AppNavigator.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "../screens/HomeScreen";
+import LoginScreen from "../screens/LoginScreen";
+import SignUpScreen from "../screens/SignUpScreen";
+import ShareScreen from "../screens/ShareScreen";
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Share" component={ShareScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
